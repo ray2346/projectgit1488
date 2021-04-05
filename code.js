b19d7884e63d3eacc6a01e25ccf1065fe8bf2b9f
Ivan <tooshka4@yandex.ru>
2021-04-05 21:00:02 +0300

    first movies

--- code.js
+++ code.js
@@ -1,7 +1,23 @@
-const a ={
-    "rtf":"86586",
-    "hgfd":"unyhyj5",
-    "ffgfg":"gfneere"
+
+
+'use strict';
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
-a.rtf=800;
-console.log(a.rtf);
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c]= d;
+
+console.log(personalMovieDB);
