72a1d9e018ebfb65091c0055ad3a7b09d1fcef0b
Ivan <tooshka4@yandex.ru>
2021-05-14 15:06:45 +0300

    25

--- code.js
+++ code.js
@@ -2,22 +2,8 @@
 
 'use strict';
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c]= d;
-
-console.log(personalMovieDB);
+if (4==119){
+    console.log('Ok!');
+} else{
+    console.log('errror');
+}
