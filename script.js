ef7d7327f80a2e2a1cc03789bd091d90df6c50ad
Ivan <tooshka4@yandex.ru>
2021-06-28 17:27:18 +0300

    перекинул с рабочего стола

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+const btns = document.querySelectorAll('button');
+// console.log(btns[0].classList.length);
+console.log(btns[1].classList.add('red'));
+// if (btns[1].classList.contains('red')){
+// console.log('red');
+// }
+
+btns[0].addEventListener('click',()=>{
+// if(!btns[1].classList.contains('red')){
+//     btns[1].classList.add('red');
+// }else  {btns[1].classList.remove('red');}
+
+btns[1].classList.toggle('red');
+// });
